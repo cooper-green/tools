77a559fee38e71d8348140c3de7ce000e483f4ee
cooper-green <cooper_17@yahoo.com>
2016-12-30 12:53:04 +0800

    Create P.java

---
+++ P.java
@@ -0,0 +1,15 @@
+package online.sanbao.tools;
+
+public class P {
+	public static void rintln(String str){
+		System.out.println(str);
+	}
+	public static void rint(String str){
+		System.out.print(str);
+	}
+	public static void rintError(String str){
+		System.out.println(str);;
+	}
+//	public static void main(String[] args){
+//		}
+}
